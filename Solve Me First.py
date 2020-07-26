97109ac7816a1cb45322b41850a07bc9b3ece53b
Deniz Akpınar <ddenizakpinar@gmail.com>
2020-07-26 18:31:20 +0300

    Create Solve Me First.py

---
+++ Solve Me First.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/solve-me-first/problem
+# 26.07.2020
+
+
+def solveMeFirst(a, b):
+    return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+print(solveMeFirst(num1,num2))
