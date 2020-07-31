b1475532bb6fb7fa1a20b6e6068281557fc9c61e
Deniz Akpınar <ddenizakpinar@gmail.com>
2020-07-31 22:13:30 +0300

    Create Smart Number.py

---
+++ Smart Number.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/smart-number/problem
+# 31.07.2020
+
+
+import math
+
+def is_smart_number(num):
+    val = int(math.sqrt(num))
+    if num / (val * val) == 1:
+        return True
+    return False
+
+for _ in range(int(input())):
+    num = int(input())
+    ans = is_smart_number(num)
+    if ans:
+        print("YES")
+    else:
+        print("NO")
