9554c448a93d94ecf1553343c796d8f6c8220d47
Deniz Akpınar <ddenizakpinar@gmail.com>
2021-06-05 17:41:45 +0300

    Create Introduction to Sets.py

---
+++ Introduction to Sets.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/py-introduction-to-sets
+
+def average(array):
+    distinct_heights = set(array)
+
+    return sum(distinct_heights) / len(distinct_heights)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
